136487ac3b0554e42e1f1519bdc3c184ead6dc65
MertKumbasar <kumbasarmert@gmail.com>
2024-05-27 20:00:52 +0300

    create room and display room pages has been created !

--- RazorPages/obj/Debug/net8.0/RazorPages.AssemblyInfo.cs
+++ RazorPages/obj/Debug/net8.0/RazorPages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8f5f88c942f246935736d54f3c96381d3703b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1823ea8317dfbc081fb25192dbe882fbd16a4c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
